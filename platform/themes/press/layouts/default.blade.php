80c1c93e2cd51e9eba9fb9ed255e04653f6ff04f
Nong Van Khanh <khanhnongvan0@gmail.com>
2020-09-12 09:02:15 +0700

    day len git ngay 12.9

--- platform/themes/press/layouts/default.blade.php
+++ platform/themes/press/layouts/default.blade.php
@@ -4,11 +4,14 @@
         <meta charset="utf-8">
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-{{--        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-s calable=no" name="viewport">--}}
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-s calable=no" name="viewport">
+{{--        {{dd("fgasdkjf")}}--}}
 
+        <link rel="icon" type="image/png" href="{{ get_image_url(theme_option('favicon')) }}"/>
         <link rel="canonical" href="{{ url('/') }}">
         <meta http-equiv="content-language" content="en">
-        <meta property="og:image" content="{{theme_option('logo_thumb')}}" />
+{{--        <meta property="og:image" content="{{theme_option('logo_thumb')}}" />--}}
+        <meta property="og:image" content="{{ get_image_url(theme_option('logo_thumb')) }}" />
         <title>{{ SeoHelper::getTitle() }}</title>
         <style>
             body{
